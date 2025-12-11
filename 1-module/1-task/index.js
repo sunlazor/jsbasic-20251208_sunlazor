8f1c3c47d339f258d5656c677c95c2fd9a5b3227
sunlazor <1979844+sunlazor@users.noreply.github.com>
2025-12-11 20:41:07 +0100

    Adds implementation of 1-1 task about factorial calculation

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,15 @@
 function factorial(n) {
-  // ваш код...
+  switch (n) {
+    case 0:
+      return 1;
+    case 1:
+      return 1;
+    default:
+      let result = 1;
+      while(n >= 2) {
+        result *= n;
+        n--;
+      }
+      return result;
+  }
 }
