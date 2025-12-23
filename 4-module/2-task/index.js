8fa6f8af90cc1cfc076f995d28afa465e1013d7c
sunlazor <1979844+sunlazor@users.noreply.github.com>
2025-12-23 20:18:16 +0100

    Adds fix of style of 4-2 task about red style of diagonal cells

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -1,7 +1,7 @@
 function makeDiagonalRed(table) {
   let i = 0;
   for (let row of table.rows) {
-    row.cells[i].style.color = 'red';
+    row.cells[i].style.backgroundColor = 'red';
     if (row.cells[i] !== undefined) {
       i++;
     }
