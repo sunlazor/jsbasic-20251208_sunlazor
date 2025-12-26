ba7757bb1393be4e1672624a69075136ce724e2f
sunlazor <1979844+sunlazor@users.noreply.github.com>
2025-12-26 22:23:40 +0100

    Adds implementation of 5-2 task about toggling a text div visibility

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,8 @@
 function toggleText() {
-  // ваш код...
+  let btn = document.querySelector('.toggle-text-button');
+  let txt = document.querySelector('#text');
+
+  if (btn) {
+    btn.addEventListener('click', () => txt.toggleAttribute('hidden'));
+  }
 }
