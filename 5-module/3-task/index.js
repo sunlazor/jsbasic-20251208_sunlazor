9d2c70ad8c336199be9da69cf379f97da3276c6f
sunlazor <1979844+sunlazor@users.noreply.github.com>
2025-12-29 18:18:01 +0100

    Fixing for tests of 5-3 task about carousel

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,7 +1,5 @@
 function initCarousel() {
-  document.addEventListener('DOMContentLoaded', () => {
-    document.querySelector('.carousel__arrow_left').style.visibility = 'hidden';
-  })
+  document.querySelector('.carousel__arrow_left').style.display = 'none'
 
   let carousel = document.querySelector('.carousel');
   var currentItem = currentItem === undefined ? 0 : currentItem;
@@ -13,25 +11,25 @@ function initCarousel() {
     if (event.target.closest('.carousel__arrow_left')) {
       if (currentItem > 0) {
         currentItem = currentItem - 1 >= 0 ? currentItem - 1 : currentItem;
-        caroInner.style.transform = `translateX(-${currentItem * 100}%)`;
+        caroInner.style.transform = `translateX(-${currentItem * 500}px)`;
       }
     } else if (event.target.closest('.carousel__arrow_right')) {
       if (currentItem < shopItemsCount) {
         currentItem = currentItem + 1 < shopItemsCount ? currentItem + 1 : currentItem;
-        caroInner.style.transform = `translateX(-${currentItem * 100}%)`;
+        caroInner.style.transform = `translateX(-${currentItem * 500}px)`;
       }
     }
 
     if (currentItem === 0) {
-      carousel.querySelector('.carousel__arrow_left').style.visibility = 'hidden';
+      carousel.querySelector('.carousel__arrow_left').style.display = 'none';
     } else {
-      carousel.querySelector('.carousel__arrow_left').style.visibility = 'visible';
+      carousel.querySelector('.carousel__arrow_left').style.display = '';
     }
 
     if (currentItem < shopItemsCount - 1) {
-      carousel.querySelector('.carousel__arrow_right').style.visibility = 'visible';
+      carousel.querySelector('.carousel__arrow_right').style.display = '';
     } else {
-      carousel.querySelector('.carousel__arrow_right').style.visibility = 'hidden';
+      carousel.querySelector('.carousel__arrow_right').style.display = 'none';
     }
   })
 }
