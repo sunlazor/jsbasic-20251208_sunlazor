714aff80d7b0e48f544b4b3674726cf7b007c369
sunlazor <1979844+sunlazor@users.noreply.github.com>
2026-01-17 01:24:35 +0100

    Adds implementation of 6-1 task about list and row deletion

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -13,6 +13,50 @@
  *
  */
 export default class UserTable {
+  elem;
+
   constructor(rows) {
+    let table = document.createElement('table');
+    let thead = table.createTHead();
+    this.#makeHeadRow(thead);
+    this.#addDataToTable(rows, table);
+
+    this.elem = table;
+  }
+
+  #makeHeadRow(thead) {
+    let hrow = thead.insertRow();
+    let th = document.createElement('TH');
+    th.textContent = 'Имя';
+    hrow.appendChild(th);
+    th = document.createElement('TH');
+    th.textContent = 'Возраст';
+    hrow.appendChild(th);
+    th = document.createElement('TH');
+    th.textContent = 'Зарплата';
+    hrow.appendChild(th);
+    th = document.createElement('TH');
+    th.textContent = 'Возраст';
+    hrow.appendChild(th);
+    let bb = document.createElement('TH');
+    bb.textContent = 'Попрощаться';
+    hrow.appendChild(bb);
+  }
+
+  #addDataToTable(rows, table) {
+    rows.forEach(function (record) {
+      let trow = table.insertRow();
+      trow.addEventListener('click', function (ev) {
+        ev.target.closest('tr').remove()
+      });
+      trow.insertCell().textContent = record.name;
+      trow.insertCell().textContent = record.age;
+      trow.insertCell().textContent = record.salary;
+      trow.insertCell().textContent = record.city;
+      let bb = trow.insertCell();
+      let button = document.createElement('BUTTON');
+      button.textContent = '×';
+      bb.appendChild(button);
+    })
   }
 }
