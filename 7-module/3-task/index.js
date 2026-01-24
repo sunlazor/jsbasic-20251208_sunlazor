09f0e1a7aba57752d7d62e82b4243cbe9a290884
sunlazor <1979844+sunlazor@users.noreply.github.com>
2026-01-24 11:56:27 +0100

    task 7-3; Adds basic html

--- 7-module/3-task/index.js
+++ 7-module/3-task/index.js
@@ -1,4 +1,37 @@
 export default class StepSlider {
+  elem;
   constructor({ steps, value = 0 }) {
+    this.elem = this.#makeSlider();
   }
+
+  #makeSlider() {
+    return this.#createElement(`
+    <!--Корневой элемент слайдера-->
+    <div class="slider">
+
+      <!--Ползунок слайдера с активным значением-->
+      <div class="slider__thumb" style="left: 50%;">
+        <span class="slider__value">2</span>
+      </div>
+
+      <!--Заполненная часть слайдера-->
+      <div class="slider__progress" style="width: 50%;"></div>
+
+      <!--Шаги слайдера-->
+      <div class="slider__steps">
+        <span></span>
+        <span></span>
+        <span class="slider__step-active"></span>
+        <span></span>
+        <span></span>
+      </div>
+    </div>
+    `);
+  }
+
+  #createElement(html) {
+    const div = document.createElement('div');
+    div.innerHTML = html;
+    return div.firstElementChild;
+  };
 }
