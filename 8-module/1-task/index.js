26c72d4fcbcdc32dbab9f130fce7293f08ee8d78
sunlazor <1979844+sunlazor@users.noreply.github.com>
2026-02-05 22:28:53 +0100

    task 8-1 Cart icon; First attempt

--- 8-module/1-task/index.js
+++ 8-module/1-task/index.js
@@ -1,6 +1,8 @@
 import createElement from '../../assets/lib/create-element.js';
 
 export default class CartIcon {
+  startPosition;
+
   constructor() {
     this.render();
 
@@ -39,6 +41,28 @@ export default class CartIcon {
   }
 
   updatePosition() {
-    // ваш код ...
+    if (!this.startPosition) {
+      const iconNode = this.elem.getBoundingClientRect();
+      this.startPosition = {left: iconNode.left, top: iconNode.top};
+    }
+
+    // console.log(`offsetTop: ${window.pageYOffset}`);
+    if (window.pageYOffset > this.startPosition.top) {
+      this.elem.style.position = 'fixed';
+      this.elem.y = '50px';
+      // this.elem.x = '20px';
+      // this.elem.x = document.documentElement.clientWidth - this.elem.offsetWidth - 10;
+      // this.elem.x = Math.round(document.querySelector('.container').getBoundingClientRect().right) + 20 + 'px';
+      // this.elem.x = Math.round(document.querySelector('.container').getBoundingClientRect().right) + 20;
+      this.elem.x = Math.min(
+        document.querySelector('.container').getBoundingClientRect().right + 20,
+        document.documentElement.clientWidth - this.elem.offsetWidth - 10
+      ) + 'px';
+
+      console.log(`cont right: ${Math.round(document.querySelector('.container').getBoundingClientRect().right)}`);
+      console.log(`elem x: ${this.elem.x}`);
+    } else {
+      this.elem.style.position = '';
+    }
   }
 }
