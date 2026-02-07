8a66002a3e51cc2a83d31f7295109ab49eae69f7
sunlazor <1979844+sunlazor@users.noreply.github.com>
2026-02-07 20:49:07 +0100

    task 8-2 ProductCard; Adds filters

--- 8-module/2-task/index.js
+++ 8-module/2-task/index.js
@@ -14,7 +14,12 @@ export default class ProductGrid {
       </div>
     `);
 
-    this.#addProductCards(this.elem, this.products);
+    this.#addProductCards();
+  }
+
+  updateFilter(filters) {
+    this.filters = filters;
+    this.#addProductCards();
   }
 
   #createElement(html) {
@@ -23,10 +28,46 @@ export default class ProductGrid {
     return div.firstElementChild;
   }
 
-  #addProductCards(prodGrid, products) {
-    let inner = prodGrid.querySelector('.products-grid__inner');
-    products.forEach(product => {
-      inner.appendChild(new ProductCard(product).elem);
-    });
+  #addProductCards() {
+    let inner = this.elem.querySelector('.products-grid__inner');
+    let filterKeys = Object.keys(this.filters);
+    let productsNodes;
+
+    if (filterKeys.length === 0) {
+      productsNodes = Array(this.products.length);
+      this.products.forEach((product, i) => {
+        productsNodes[i] = new ProductCard(product).elem;
+      });
+    } else {
+      productsNodes = [];
+      this.products.forEach(product => {
+        console.log(this.filters);
+        // 1. Без орехов
+        if (Object.hasOwn(this.filters, 'noNuts') && this.filters['noNuts'] === true
+          && Object.hasOwn(product, 'nuts') && product['nuts'] === true) {
+          return;
+        }
+        // 2. Вегетарианское
+        if (Object.hasOwn(this.filters, 'vegeterianOnly') && this.filters['vegeterianOnly'] === true
+          && Object.hasOwn(product, 'vegeterian') && product['vegeterian'] !== true) {
+          return;
+        }
+        // 3. Максимальная острота
+        if (Object.hasOwn(this.filters, 'maxSpiciness') && this.filters['maxSpiciness'] !== undefined
+          && Object.hasOwn(product, 'spiciness') && product['spiciness'] > this.filters['maxSpiciness']) {
+          return;
+        }
+        // 4. Категория
+        if (Object.hasOwn(this.filters, 'category') && this.filters['category']
+          && Object.hasOwn(product, 'category') && product['category'] !== this.filters['category']) {
+          return;
+        }
+
+        productsNodes.push(new ProductCard(product).elem);
+        console.log(productsNodes);
+      });
+    }
+
+    inner.replaceChildren(...productsNodes);
   }
 }
