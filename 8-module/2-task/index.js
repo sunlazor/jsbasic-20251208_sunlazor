c4227826b7a353ae1e461a18759d6173d6bac846
sunlazor <1979844+sunlazor@users.noreply.github.com>
2026-02-07 17:45:32 +0100

    task 8-2 ProductCard; Show products list

--- 8-module/2-task/index.js
+++ 8-module/2-task/index.js
@@ -8,16 +8,25 @@ export default class ProductGrid {
 
     this.elem = this.#createElement(`
       <div class="products-grid">
-      <div class="products-grid__inner">
-        <!--ВОТ ТУТ БУДУТ КАРТОЧКИ ТОВАРОВ-->
-      </div>
+        <div class="products-grid__inner">
+          <!--ВОТ ТУТ БУДУТ КАРТОЧКИ ТОВАРОВ-->
+        </div>
       </div>
     `);
+
+    this.#addProductCards(this.elem, this.products);
   }
 
   #createElement(html) {
     const div = document.createElement('div');
     div.innerHTML = html;
     return div.firstElementChild;
+  }
+
+  #addProductCards(prodGrid, products) {
+    let inner = prodGrid.querySelector('.products-grid__inner');
+    products.forEach(product => {
+      inner.appendChild(new ProductCard(product).elem);
+    });
   }
 }
