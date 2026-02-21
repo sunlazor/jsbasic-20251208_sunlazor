cf3590495cef468589806e2afe4cc089bd1c9b19
sunlazor <1979844+sunlazor@users.noreply.github.com>
2026-02-22 00:12:41 +0100

    task 8-4 CartItems part 2; 8.1 other fixes

--- 8-module/1-task/index.js
+++ 8-module/1-task/index.js
@@ -45,19 +45,25 @@ export default class CartIcon {
   }
 
   updatePosition() {
-    if (!this.startPosition) {
-      const iconNode = this.elem.getBoundingClientRect();
-      this.startPosition = {left: iconNode.left, top: iconNode.top};
+    if (document.documentElement.clientWidth <= 767) {
+      Object.assign(this.elem.style, {
+        position: '',
+        top: '',
+        left: '',
+        zIndex: ''
+      });
     }
 
-    if (document.documentElement.clientWidth > 767
-      && window.pageYOffset > this.startPosition.top
-      && document.querySelector('.header.container').getBoundingClientRect()
-    ) {
+    if (!this.initialTopCoord) {
+      this.initialTopCoord = this.elem.getBoundingClientRect().top + window.pageYOffset;
+    }
+
+    if (window.pageYOffset > this.initialTopCoord) {
       let leftIndent = Math.min(
-        document.querySelector('.header.container').getBoundingClientRect().right + 20,
+        document.querySelector('.container').getBoundingClientRect().right + 20,
         document.documentElement.clientWidth - this.elem.offsetWidth - 10
       ) + 'px';
+
       Object.assign(this.elem.style, {
         position: 'fixed',
         top: '50px',
